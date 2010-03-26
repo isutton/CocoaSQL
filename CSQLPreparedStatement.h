441861dd1dcfb860cf7f6d175f95e1c868a1806a
Igor Sutton <igor@straydev.com>
2010-03-26 14:11:57 +0100

    added basic documentation.

--- CSQLPreparedStatement.h
+++ CSQLPreparedStatement.h
@@ -9,13 +9,45 @@
 
 @protocol CSQLPreparedStatement
 
+/**
+ 
+ @param values
+ @param error
+ 
+ @return <code>success</code>
+ 
+ */
 - (BOOL)executeWithValues:(NSArray *)values error:(NSError **)error;
 
+/**
+ 
+ @param error
+ 
+ @return <code>success</code>
+ 
+ */
 - (BOOL)execute:(NSError **)error;
 
-- (NSDictionary *)fetchRowAsDictionary;
+/**
+ 
+ @param error
+ 
+ @return <code>row</code> An <code>NSDictionary</code> instance containing the
+ row contents, with the column names as keys.
+ 
+ */
 
-- (NSArray *)fetchRowAsArray;
+- (NSDictionary *)fetchRowAsDictionary:(NSError **)error;
+
+/**
+ 
+ @param error
+ 
+ @return <code>row</code> An <code>NSArray</code> instance containing the row
+ contents.
+ 
+ */
+- (NSArray *)fetchRowAsArray:(NSError **)error;
 
 @end
 
