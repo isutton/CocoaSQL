4f714dac35db5195ee3dc947d629e867b7cae38a
Igor Sutton <igor@straydev.com>
2010-03-31 14:04:06 +0200

    added unit test infrastructure.

---
+++ CSQLDatabaseTests.h
@@ -0,0 +1,16 @@
+//
+//  CSQLDatabaseTests.h
+//  CocoaSQL
+//
+//  Created by Igor Sutton on 3/31/10.
+//  Copyright 2010 CocoaSQL.org. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface CSQLDatabaseTests : SenTestCase {
+
+}
+
+@end
---
+++ CSQLDatabaseTests.m
@@ -0,0 +1,14 @@
+//
+//  CSQLDatabaseTests.m
+//  CocoaSQL
+//
+//  Created by Igor Sutton on 3/31/10.
+//  Copyright 2010 CocoaSQL.org. All rights reserved.
+//
+
+#import "CSQLDatabaseTests.h"
+
+
+@implementation CSQLDatabaseTests
+
+@end
