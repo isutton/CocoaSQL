049a2c67a92ad66b83cd612f172a267aaeaa898d
Igor Sutton <igor@straydev.com>
2010-04-06 14:47:07 +0200

    added CSQLPreparedStatement.m.

---
+++ Source/CocoaSQL/CSQLPreparedStatement.m
@@ -0,0 +1,16 @@
+//
+//  CSQLPreparedStatement.m
+//  CocoaSQL
+//
+//  Created by Igor Sutton on 4/6/10.
+//  Copyright 2010 CocoaSQL.org. All rights reserved.
+//
+
+#import "CSQLPreparedStatement.h"
+
+@implementation CSQLPreparedStatement
+
+@synthesize database;
+@synthesize canFetch;
+
+@end
