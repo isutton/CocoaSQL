c18c9f62ef81c7d19b92694406891036f9a37859
Igor Sutton <igor@straydev.com>
2010-04-09 11:52:05 +0200

    added our own test rig.
    The default test rig seems to be broken, so I added our own. More information about it can be found here:
    
    http://www.dribin.org/dave/blog/archives/2006/01/26/test_bundle_3/
    http://lists.apple.com/archives/Cocoa-dev/2007/Nov/msg00851.html
    http://chanson.livejournal.com/119578.html

---
+++ otest.m
@@ -0,0 +1,39 @@
+/*
+ *  otest.c
+ *  CocoaSQL
+ *
+ *  Created by Igor Sutton on 4/9/10.
+ *  Copyright 2010 CocoaSQL.org. All rights reserved.
+ *
+ */
+
+
+#import <Cocoa/Cocoa.h>
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SenTestProbe (InternalMethods)
+- (void)runTests:(id)ignored;
+@end
+
+int main(int argc, char **argv) {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    // Tell SenTestingKit to use our last argument as the test bundle, to
+    // run all the tests it can find, and that the executable is equivalent
+    // to the otest test rig supplied with OCUnit.
+    
+    NSDictionary *testDefaults = [NSDictionary dictionaryWithObjectsAndKeys:
+                                  [[[NSProcessInfo processInfo] arguments] lastObject], SenTestedUnitPath,
+                                  SenTestScopeAll, SenTestScopeKey,
+                                  [NSNumber numberWithBool:YES], SenTestToolKey,
+                                  nil];
+    [[NSUserDefaults standardUserDefaults] registerDefaults:testDefaults];
+    
+    // Run the tests based on the defaults set above.
+    // It will invoke exit() with an appropriate value as well.
+    
+    [SenTestProbe runTests:nil];
+    
+    [pool drain];
+    return 0;
+}
