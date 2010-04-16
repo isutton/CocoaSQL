bed8987a0665e3d5680a1f5493e47583ce2ffe8b
Igor Sutton <igor@straydev.com>
2010-04-16 15:22:55 +0200

    more prepared statement code. WIP.
    	Check the comments in the code for further information.

--- Source/PostgreSQL/CSPostgreSQLPreparedStatement.m
+++ Source/PostgreSQL/CSPostgreSQLPreparedStatement.m
@@ -20,7 +20,38 @@
 
 #import "CSPostgreSQLPreparedStatement.h"
 #import "CSQLResultValue.h"
-#import <libpq-fe.h>
+#include <libpq-fe.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+//
+// TODO: Find the proper way to wire PostgreSQL basic data types.
+//
+#define BYTEAOID   17
+#define CHAROID    18
+#define TEXTOID    25
+#define INT8OID    20
+#define INT2OID    21
+#define INT4OID    23
+#define NUMERICOID 1700
+#define VARCHAROID 1043
+
+@interface CSPostgreSQLBindsStorage : NSObject
+{
+    int type;
+    int length;
+    char *value;
+}
+
+@end
+
+
+@implementation CSPostgreSQLBindsStorage
+
+
+@end
+
 
 @implementation CSPostgreSQLPreparedStatement
 
@@ -98,38 +129,84 @@
     const char **paramValues = nil;
     int *paramLengths = nil; // don't need param lengths since text
     int *paramFormats = nil; // default to all text params
-    int resultFormat = 0;    // ask for text results
+    int resultFormat = 0;    // ask for binary results
+    
+    int *mallocBookKeeping = nil;
     
     //
     // TODO: prepare values to feed PQexecPrepared.
     //
     if (values && [values count] > 0) {
         nParams = [values count];
-        paramValues = malloc(nParams * sizeof(*paramValues));
+        paramValues = malloc(nParams * sizeof(char *));
+        mallocBookKeeping = calloc(nParams, sizeof(int));
+
         if (resultFormat) {
             paramLengths = calloc(nParams, sizeof(int));
             paramFormats = calloc(nParams, sizeof(int));
         }
-        
+
         for (int i = 0; i < nParams; i++) {
             id value = [values objectAtIndex:i];
             
             if ([[value class] isSubclassOfClass:[NSNumber class]]) {
-                paramValues[i] = [[(NSNumber *)value stringValue] UTF8String];
+                if (resultFormat) {
+                    uint32_t value_ = htonl((uint32_t)[value intValue]);
+                    paramFormats[i] = 1;
+                    paramLengths[i] = sizeof(uint32_t);
+                    paramValues[i] = (char *)&value_;
+                }
+                else {
+                    paramValues[i] = [[value stringValue] UTF8String];
+                }
+
             }
             else if ([[value class] isSubclassOfClass:[NSString class]]) {
-                paramValues[i] = [(NSString *)value UTF8String];
+                if (resultFormat) {
+                    paramFormats[i] = 1;
+                    paramLengths[i] = sizeof([[value dataUsingEncoding:NSASCIIStringEncoding] bytes]);
+                    paramValues[i] = [[value dataUsingEncoding:NSASCIIStringEncoding] bytes];
+                }
+                else {
+                    paramValues[i] = [value cStringUsingEncoding:NSASCIIStringEncoding];
+                }
             }
             else if ([[value class] isSubclassOfClass:[NSData class]]) {
                 if (resultFormat) {
                     paramFormats[i] = 1; // binary
                     paramLengths[i] = [value length];
                 }
+                
+#if 0
+                //
+                // For some reason, if I use
+                //
+                // paramValues[i] = [value bytes]
+                //
+                // It is given an extra byte (probably the '\0' character, which is
+                // interpreted by the database as space. The following might be wrong,
+                // but I got better results malloc'ing and copying the bytes using
+                // getBytes:length:
+                //
+                // I suspect something is leaking here, because the tests randomly 
+                // passes.
+                //
+
+                mallocBookKeeping[i] = 1;
+                void *buffer = malloc([value length]);
+                [value getBytes:buffer length:([value length] * sizeof(char))];
+                paramValues[i] = buffer;
+#else
+                // 
+                // Here we always get the extra byte, but at least it is consistent. Another
+                // thing is that it always fails with the GC on.
+                //
                 paramValues[i] = [value bytes];
+#endif
             }
         }
     }
-
+    
     PGresult *result = PQexecPrepared(database.databaseHandle, 
                                       "",
                                       nParams, 
@@ -139,6 +216,11 @@
                                       resultFormat);
     
     if (values && [values count] > 0) {
+        for (int i = 0; i < [values count]; i++) {
+            if (mallocBookKeeping[i]) {
+                free((char *)paramValues[i]);
+            }
+        }
         free(paramValues);
         if (resultFormat) {
             free(paramLengths);
@@ -181,21 +263,60 @@
         return nil;
     }
     
-    NSMutableDictionary *row = [NSMutableDictionary dictionaryWithCapacity:numFields];
+    PGresult *result = PQdescribePrepared(database.databaseHandle, "");
     
+    NSMutableDictionary *row = [NSMutableDictionary dictionaryWithCapacity:numFields];
+
+    //
+    // The following block is the same as in fetchRowAsArray:error:. We need to refactor
+    // it so both messages can use.
+    //
     for (int i = 0; i < numFields; i++) {
         CSQLResultValue *value;
+        int type = PQftype(statement, i);
+        int length_ = PQgetlength(statement, currentRow, i);
+        char *value_ = PQgetvalue(statement, currentRow, i);
+        
+        if (PQfformat(statement, i)) {
+            switch (type) {
+                case BYTEAOID:
+                    value = [CSQLResultValue valueWithData:[NSData dataWithBytes:value_ length:length_-sizeof(char *)]];
+                    break;
+                case CHAROID:
+                case TEXTOID:
+                case VARCHAROID:
+                    value = [CSQLResultValue valueWithUTF8String:value_];
+                    break;
+                case INT8OID:
+                case INT4OID:
+                case INT2OID:
+                case NUMERICOID:
+                    value = [CSQLResultValue valueWithNumber:[NSNumber numberWithInt:ntohl(*(int *)value_)]];
+                default:
+                    break;
+            }
+        }            
+        else {
+            switch (type) {
+                case BYTEAOID:
+                    value = [CSQLResultValue valueWithData:[NSData dataWithBytes:value_ length:length_]];
+                    break;
+                default:
+                    value = [CSQLResultValue valueWithUTF8String:value_];        
+                    break;
+            }
+        }
         
         NSString *key = [NSString stringWithFormat:@"%s", PQfname(statement, i)];
-        /* TODO - fix once using resultFormat == 1 
-        if (PQfformat(statement, i))
-            value = [CSQLResultValue valueWithData:[NSData dataWithBytes:PQgetvalue(statement, currentRow, i) length:PQfsize(statement, i)]];
-        else*/
-            value = [CSQLResultValue valueWithUTF8String:PQgetvalue(statement, currentRow, i)];
 
         [row setObject:value forKey:key];
     }
 
+    if (result) {
+        PQclear(result);
+        result = nil;
+    }
+    
     currentRow++;
     
     return row;
@@ -218,12 +339,40 @@
     
     for (int i = 0; i < numFields; i++) {
         CSQLResultValue *value;
-        /* TODO - fix once using resultFormat == 1 
-        if (PQfformat(statement, i))
-            value = [CSQLResultValue valueWithData:[NSData dataWithBytes:PQgetvalue(statement, currentRow, i) length:PQfsize(statement, i)]];
-        else*/
-            value = [CSQLResultValue valueWithUTF8String:PQgetvalue(statement, currentRow, i)];
-
+        int type = PQftype(statement, i);
+        int length_ = PQgetlength(statement, currentRow, i);
+        char *value_ = PQgetvalue(statement, currentRow, i);
+        
+        if (PQfformat(statement, i)) {
+            switch (type) {
+                case BYTEAOID:
+                    value = [CSQLResultValue valueWithData:[NSData dataWithBytes:value_ length:length_-sizeof(char *)]];
+                    break;
+                case CHAROID:
+                case TEXTOID:
+                case VARCHAROID:
+                    value = [CSQLResultValue valueWithUTF8String:value_];
+                    break;
+                case INT8OID:
+                case INT4OID:
+                case INT2OID:
+                case NUMERICOID:
+                    value = [CSQLResultValue valueWithNumber:[NSNumber numberWithInt:ntohl(*(int *)value_)]];
+                default:
+                    break;
+            }
+        }            
+        else {
+            switch (type) {
+                case BYTEAOID:
+                    value = [CSQLResultValue valueWithData:[NSData dataWithBytes:value_ length:length_]];
+                    break;
+                default:
+                    value = [CSQLResultValue valueWithUTF8String:value_];        
+                    break;
+            }
+        }
+        
         [row addObject:value];
     }
     
