a5cc49b519599c54101e63fe8f38fd72a72e0b42
Igor Sutton <igor@straydev.com>
2010-07-05 16:17:58 +0200

    removed CSMySQLDatabase.h import.

--- otest.m
+++ otest.m
@@ -20,8 +20,6 @@
 
 #import <Cocoa/Cocoa.h>
 #import <SenTestingKit/SenTestingKit.h>
-#import "CSMySQLDatabase.h"
-
 
 int main(int argc, char **argv) {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
