3d0f4173f90fe296fea8333eb7cc2d792f567a8f
Igor Sutton <igorsutton@me.com>
2012-02-20 00:00:43 +0100

    Remove test targets to add tests over again using Xcode 4.* tests infra-structure.

---
+++ CocoaSQL-Tests/CocoaSQL_Tests.h
@@ -0,0 +1,13 @@
+//
+//  CocoaSQL_Tests.h
+//  CocoaSQL-Tests
+//
+//  Created by Igor Sutton on 2/19/12.
+//  Copyright (c) 2012 igorsutton.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CocoaSQL_Tests : SenTestCase
+
+@end
---
+++ CocoaSQL-Tests/CocoaSQL_Tests.m
@@ -0,0 +1,32 @@
+//
+//  CocoaSQL_Tests.m
+//  CocoaSQL-Tests
+//
+//  Created by Igor Sutton on 2/19/12.
+//  Copyright (c) 2012 igorsutton.com. All rights reserved.
+//
+
+#import "CocoaSQL_Tests.h"
+
+@implementation CocoaSQL_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CocoaSQL-Tests");
+}
+
+@end
